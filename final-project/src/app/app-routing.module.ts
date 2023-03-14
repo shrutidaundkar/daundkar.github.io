394f7fd1d8ec1614003f158cbd0b39ef2283a860
shrutidaundkar <shrutivd135@gmail.com>
2023-03-14 08:07:05 -0700

    Added content inside about cmp and updated navbar

--- final-project/src/app/app-routing.module.ts
+++ final-project/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { PreviousWorkComponent } from './previous-work/previous-work.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   {path:'', component: AboutComponent},
   {path:'previous-work', component: PreviousWorkComponent},
-  {path:'projects', component: AboutComponent},  
+  {path:'projects', component: ProjectsComponent},  
   {path:'contact', component: ContactComponent},  
 ];
 
